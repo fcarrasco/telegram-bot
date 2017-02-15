e7f877e6b7146e7540bed9ac56974ea6b2dfb9d7
Felipe Carrasco <elrepro@gmail.com>
2017-02-15 19:06:28 -0200

    change port

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
     app.logger.addHandler(handler)
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
